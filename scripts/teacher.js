dc165293f5911a5466f0ed8c52135c079d65e3a2
MattCordeiro <matthew3cord@gmail.com>
2015-01-06 14:58:26 -0800

    Teacher example

---
+++ scripts/teacher.js
@@ -0,0 +1,13 @@
+var isTeacher = function(name) {
+  if (name === 'Dan' || name === 'Andrew') {
+    console.log('true');
+    return true;
+  }
+  else { 
+    console.log('false');
+    return false;
+  }
+}
+//isTeacher('Dan');
+// isTeacher('Andrew');
+isTeacher('Dick');
