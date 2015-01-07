1aabbf855a1156d859b8b594d3ad7bbe5cc928bf
MattCordeiro <matthew3cord@gmail.com>
2015-01-06 16:06:17 -0800

    Day one challenges

---
+++ scripts/challenge1.js
@@ -0,0 +1,21 @@
+var mySum = function(numOne, numTwo) {
+  console.log('The sum of ' + numOne + ' and ' + numTwo + ' is ' + (numOne + numTwo) + '.');
+}
+mySum(5, 175);
+// mySum('nine', 4);
+
+var myDiff = function(numOne, numTwo) {
+  console.log('The diference between ' + numOne + ' and ' + numTwo + ' is ' + (numOne - numTwo) + '.');
+}
+myDiff(171, 72);
+
+var meetUp = function(personOne, personTwo) {
+  console.log('When ' + personOne + ' met ' + personTwo + ', it was electric!');
+}
+meetUp('Luke', 'Leia');
+
+var myDiff = function(numOne, numTwo) {
+  console.log('The diference between ' + numOne + ' and ' + numTwo + ' is ' + Math.abs(numOne - numTwo) + '.');
+}
+myDiff(171, 72);
+
