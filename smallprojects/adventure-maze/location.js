ee954be8ec7d248f42151ea35af06271d1bdaaf8
MattCordeiro <matthew3cord@gmail.com>
2015-01-12 23:03:17 -0800

    Working on location, returning object resolved.

--- smallprojects/adventure-maze/location.js
+++ smallprojects/adventure-maze/location.js
@@ -1,9 +1,34 @@
 // location.js  
 
-exports.jsonLocation = function(){
-  console.log('json');
+var gameMap = require('./game.json');
+
+exports.currentRoom = function(roomName){
+  
+
+
+  function currentCallback(element, index, array) {
+//    console.log(array[index].name);
+//   console.log(roomName);
+
+  if(roomName === array[index].name){
+    //var currentKeys = Object.keys(room);
+    var thisRoom = element; 
+    console.log(thisRoom); 
+    //console.log(currentKeys);
+  }
+ //   console.log('none' );
+  }
+//  console.log(gameMap.rooms[0].name);
+  
+  (gameMap.rooms).forEach(currentCallback);
+// which room  
 };
 
-exports.newjsonLocation = function(){
+exports.currentRoomProperties = function(){
   console.log('new');
+  // properties of room  
+  // number of doors  
+  // directions of doors  
 };
+
+
---
+++ smallprojects/adventure-maze/message.js
@@ -0,0 +1,11 @@
+// message.js
+
+exports.message = function(roomObj){
+  console.log(roomObj);
+  // number of doors  
+  // direction of doors  
+  // prompt
+  // callback
+};
+
+
