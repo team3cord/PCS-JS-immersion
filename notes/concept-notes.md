bdc48b09e2203df6a57d3425a8c2811b1745633e
MattCordeiro <matthew3cord@gmail.com>
2015-01-14 10:24:09 -0800

    Updated notes. Challenge One complete.

--- notes/concept-notes.md
+++ notes/concept-notes.md
@@ -1,4 +1,18 @@
 # Notes on Key Concepts  
 
-  * ?  
+// WHY DOESN'T THIS WORK!!! 
+var key = 1;
+var value = 'north';
+var obj = jsonMap[1];
+console.log((gameMap.rooms[key].value)); // undefined  
+console.log(jsonMap[1].value);  // undefined  
+console.log(jsonMap[key].north); //  E 
+console.log(obj[value]); // E
+
+// WHY DOESN'T THIS WORK!!! 
+
+* Dot notation can't accept a variable. You have to use the []notation.  
+* 
+
+
 
---
+++ scripts/closure.js
@@ -0,0 +1,31 @@
+/* var otherName = 'Google';
+function init() {
+  var name = 'MDN';
+  function displayName() {
+    console.log(name);  
+    console.log(otherName);
+  }
+  displayName();
+
+}
+init();
+function initTwo() {
+  function displayNameTwo() {
+    console.log(name);  
+    console.log(otherName);
+  }
+  displayNameTwo();
+
+}
+initTwo();
+*/
+
+var num = 0;
+function sequence(){
+  num ++;
+    console.log(num);
+}
+sequence();
+sequence();
+sequence();
+sequence();
