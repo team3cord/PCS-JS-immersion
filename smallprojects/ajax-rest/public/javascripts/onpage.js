8af5c8b3d41550cea5a1164e65aa95b82726e2c5
MattCordeiro <matthew3cord@gmail.com>
2015-01-22 16:01:27 -0800

    Working flicker api call.

--- smallprojects/ajax-rest/public/javascripts/onpage.js
+++ smallprojects/ajax-rest/public/javascripts/onpage.js
@@ -1,11 +1,24 @@
-(function(){
+
   var request = new XMLHttpRequest();
   request.onload = function(){
-    ourAjax = this.responseText;
+    var ourAjax = this.responseText;
     jsonObj = JSON.parse(ourAjax);
-    console.log(jsonObj.repository['type']);
-    console.log(ourAjax); 
+    console.log(jsonObj.repository['type']); // or as jsonObj.repostitory.type dot notation
+    //onsole.log(ourAjax); 
   };
   request.open('get', '/getme.json');
   request.send();
-})();
+
+  // --------------------------------------------- flicker ------;
+  
+  var scriptTag = document.createElement('script');
+
+  scriptTag.src = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&name=value&api_key=502bddfc0a6ded77873b2aa01dfafec8&user_id=129077414%40N05&format=json&jsoncallback=doIt';
+ 
+  document.body.appendChild(scriptTag);
+
+window.doIt = function(data){
+  console.log(data.photos.photo[0].id);
+
+};
+  
