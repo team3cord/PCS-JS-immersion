edd7ab9645acc1c8e7260c1a6064a968024d41e1
MattCordeiro <matthew3cord@gmail.com>
2015-01-23 17:59:11 -0800

    Updated api key.

--- smallprojects/ajax-rest/public/javascripts/onpage.js
+++ smallprojects/ajax-rest/public/javascripts/onpage.js
@@ -13,12 +13,12 @@
   
   var scriptTag = document.createElement('script');
 
-  scriptTag.src = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&name=value&api_key=502bddfc0a6ded77873b2aa01dfafec8&user_id=129077414%40N05&format=json&jsoncallback=doIt';
+  scriptTag.src = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&name=value&api_key=bd7bf82ac0a8a07f2573583543ce5856&user_id=129077414%40N05&format=json&jsoncallback=doIt';
  
   document.body.appendChild(scriptTag);
 
 window.doIt = function(data){
-  console.log(data.photos.photo[0].id);
+  console.log(data);
 
 };
   
