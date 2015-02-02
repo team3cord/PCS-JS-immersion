ca6bf5649b23967edbfc7ac1004779bc14618738
MattCordeiro <matthew3cord@gmail.com>
2015-02-02 15:50:21 -0800

    Added first version of cities migration. Need to update db tables.

--- smallprojects/databases/knexfile.js
+++ smallprojects/databases/knexfile.js
@@ -3,45 +3,16 @@
 module.exports = {
 
   development: {
-    client: 'postgrew',
+    debug: true,
+    client: 'postgres',
     connection: {
       host     : process.env.APP_DB_HOST       || '127.0.0.1',
       user     : process.env.APP_DB_USER       || '',
       password : process.env.APP_DB_PASSWORD   || '',
-      database : process.env.APP_DB_NAME       || 'jis-bookshelf'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      database : process.env.APP_DB_NAME       || 'jsi_bookshelf'
     }
   }
+
+
 
 };
---
+++ smallprojects/databases/migrations/20150202154413_cities.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cities', function(table) {
+        table.increments('id').primary();
+        table.string('name');
+        table.string(:)
+      }); 
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('cities');
+};
