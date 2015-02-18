8daab9c864e67cb051ba00fa6b44d5af170944dd
MattCordeiro <matthew3cord@gmail.com>
2015-02-17 21:34:16 -0800

    Modular working with correct callback err handling.

--- nodeschool/mod-one.js
+++ nodeschool/mod-one.js
@@ -2,10 +2,14 @@
 var modtwo = require('./mod-two');
 function callback(err, data){
   if(err){
-    callback(err);
+    console.log('error'); 
   }
   else{
-    callback(null, data);
+   //console.log(data); 
+    data.forEach(function(val,index,array){
+      console.log(val);
+    });
   }
+  
 }
 modtwo(process.argv[2],process.argv[3], callback);
--- nodeschool/mod-two.js
+++ nodeschool/mod-two.js
@@ -3,16 +3,36 @@ var fs    = require('fs'),
     path = require('path');
 
 module.exports = function(dir, ext, callback){
-  var list =fs.readdir(dir, function cbFunction(err, data){
+  fs.readdir(dir, function cbFunction(err, data){
     if(err) {
-       return callback();
+       return callback(err);
     }
     else {
+      var list = [];
       data.forEach(function(val,index,array){
         if(path.extname(val) === '.' + ext){
-          console.log(val); 
+         list.push(val);
         }
-      }); 
+
+      });
+      return callback(null,list);
     }
   });
 };
+// official solution  
+//   var fs = require('fs')
+//    var path = require('path')
+//
+//   module.exports = function (dir, filterStr, callback) {
+//
+//     fs.readdir(dir, function (err, list) {
+//      if (err)
+//       return callback(err)
+//
+//       list = list.filter(function (file) {
+//        return path.extname(file) === '.' + filterStr
+//     })
+//
+//       callback(null, list)
+//    })
+// }
